e1fa09b805b1704ec86bb936f4fca2d141cc42a2
Akshay <akshaydhumda@gmail.com>
2026-08-14 21:01:44 +0530

    feat(components): add DAC MCP4725 I2C digital-to-analog converter specs

---
+++ circuit-lab/frontend/src/constants/mcp4725Specs.js
@@ -0,0 +1,10 @@
+/**
+ * MCP4725 12-Bit I2C DAC Component Specs
+ */
+export const MCP4725_DAC_SPECS = {
+  type: 'MCP4725',
+  name: 'MCP4725 12-Bit DAC',
+  category: 'Converters',
+  i2cDefaultAddress: 0x62,
+  resolutionBits: 12
+};
