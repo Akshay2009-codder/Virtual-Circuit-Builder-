ab41f77d5b26d78ebf8ab1398e08508b652e1569
Akshay <akshaydhumda@gmail.com>
2026-08-14 21:01:45 +0530

    feat(components): add Relay Driver ULN2003 transistor array pin mapping

---
+++ circuit-lab/frontend/src/constants/uln2003Specs.js
@@ -0,0 +1,10 @@
+/**
+ * ULN2003 Transistor Array Relay Driver Specs
+ */
+export const ULN2003_SPECS = {
+  type: 'ULN2003',
+  name: 'ULN2003 Darlington Transistor Array',
+  category: 'Driver ICs',
+  channels: 7,
+  maxCollectorCurrentMilliAmps: 500
+};
