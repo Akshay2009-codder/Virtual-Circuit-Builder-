6f9cb93973e5dfec11190a19d4040c4d6a38114e
Akshay <akshaydhumda@gmail.com>
2026-08-14 20:56:53 +0530

    feat(components): add Zener diode reverse breakdown simulation parameters

---
+++ circuit-lab/frontend/src/constants/zenerDiodeSpecs.js
@@ -0,0 +1,9 @@
+/**
+ * 1N4733A Zener Voltage Regulator Diode Specs
+ */
+export const ZENER_DIODE_SPECS = {
+  model: '1N4733A',
+  zenerVoltage: 5.1,
+  powerDissipationWatt: 1.0,
+  reverseZenerResistance: 7.0
+};
