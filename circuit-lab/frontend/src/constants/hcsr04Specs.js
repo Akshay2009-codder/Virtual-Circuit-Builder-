800bffaa239172d23f03d0521c3ee0571f4a5c1a
Akshay <akshaydhumda@gmail.com>
2026-08-14 21:01:44 +0530

    feat(components): add Ultrasonic Sensor HC-SR04 pulse-echo simulation params

---
+++ circuit-lab/frontend/src/constants/hcsr04Specs.js
@@ -0,0 +1,11 @@
+/**
+ * HC-SR04 Ultrasonic Distance Sensor Specifications
+ */
+export const HCSR04_SPECS = {
+  type: 'HC_SR04',
+  name: 'Ultrasonic Ranging Module HC-SR04',
+  category: 'Sensors',
+  triggerPulseUs: 10,
+  echoSpeedOfSoundMs: 343,
+  rangeCm: [2, 400]
+};
