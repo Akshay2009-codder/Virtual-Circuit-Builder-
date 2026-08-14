e158b802e6e745036853621f41abbf084fe47154
Akshay <akshaydhumda@gmail.com>
2026-08-14 20:56:53 +0530

    feat(components): add Schottky diode fast recovery electrical characteristics

---
+++ circuit-lab/frontend/src/constants/schottkyDiodeSpecs.js
@@ -0,0 +1,10 @@
+/**
+ * 1N5819 Schottky Barrier Diode Specs
+ */
+export const SCHOTTKY_DIODE_SPECS = {
+  model: '1N5819',
+  type: 'Schottky Diode',
+  forwardDropVoltage: 0.35,
+  maxContinuousCurrent: 1.0,
+  reverseRecoveryTimeNs: 10
+};
