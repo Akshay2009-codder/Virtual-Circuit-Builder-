5436f029f5b1b4d75b41da754ec8e1c7f3a30e85
Akshay <akshaydhumda@gmail.com>
2026-08-14 21:01:43 +0530

    feat(components): add Stepper Motor Driver A4988 pinouts and microstepping specs

---
+++ circuit-lab/frontend/src/constants/a4988DriverSpecs.js
@@ -0,0 +1,10 @@
+/**
+ * A4988 Stepper Motor Driver Component Definition
+ */
+export const A4988_DRIVER_SPECS = {
+  type: 'A4988_STEPPER_DRIVER',
+  name: 'A4988 Stepper Driver Carrier',
+  category: 'Motor Drivers',
+  microstepResolutions: ['Full', '1/2', '1/4', '1/8', '1/16'],
+  maxOutputCurrentAmps: 2.0
+};
