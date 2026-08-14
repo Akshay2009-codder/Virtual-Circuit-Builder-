76a6f25834c1e961cabbf077d643c665c570b429
Akshay <akshaydhumda@gmail.com>
2026-08-14 21:01:44 +0530

    feat(components): add ESP32 Wi-Fi/Bluetooth SoC pin mapping & power profile

---
+++ circuit-lab/frontend/src/constants/esp32SocSpecs.js
@@ -0,0 +1,11 @@
+/**
+ * ESP32 WROOM-32 Microcontroller SoC Specs
+ */
+export const ESP32_SOC_SPECS = {
+  type: 'ESP32_DEV_KIT',
+  name: 'ESP32 WROOM-32 Dev Board',
+  category: 'Microcontrollers',
+  gpioCount: 30,
+  operatingVoltage: 3.3,
+  wifiFrequencyGhz: 2.4
+};
