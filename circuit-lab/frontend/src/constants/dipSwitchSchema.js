3b80c15de8974ff488c26e343b9ef1d3947ff4f3
Akshay <akshaydhumda@gmail.com>
2026-08-14 20:56:53 +0530

    feat(components): add DIP switch bank (4-position) component definitions

---
+++ circuit-lab/frontend/src/constants/dipSwitchSchema.js
@@ -0,0 +1,14 @@
+/**
+ * 4-Position DIP Switch Bank Component Schema
+ */
+export const DIP_SWITCH_SCHEMA = {
+  type: 'DIP_SWITCH_4',
+  name: '4-Position DIP Switch',
+  category: 'Switches',
+  switches: [
+    { id: 'SW1', position: false },
+    { id: 'SW2', position: false },
+    { id: 'SW3', position: false },
+    { id: 'SW4', position: false }
+  ]
+};
